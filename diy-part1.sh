bee51c649d0fe6bcc61443647f2d7569d0f26521
craytona170 <121808362+craytona170@users.noreply.github.com>
2023-01-20 04:59:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-argon-config
-sudo git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sudo git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
